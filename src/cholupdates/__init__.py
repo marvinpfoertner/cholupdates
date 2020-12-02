dab7a9c558685b38f4f968b9601c54830bb95c9e
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2020-12-02 18:06:50 +0100

    Add project scaffolding

---
+++ src/cholupdates/__init__.py
@@ -0,0 +1,5 @@
+""" This package provides algorithms which efficiently compute the Cholesky
+factorization of low-rank up-/downdates to a matrix with a known Cholesky factorization.
+"""
+
+VERSION = "0.0.1"

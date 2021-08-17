673fbde4ac5997560c6468ed75c58669ec625425
Marvin Pfoertner <marvin.pfoertner@icloud.com>
2021-08-18 01:52:31 +0200

    Import version from _version.py in __init__.py more elegantly

--- src/cholupdates/__init__.py
+++ src/cholupdates/__init__.py
@@ -3,6 +3,4 @@ factorization of low-rank up-/downdates to a matrix with a known Cholesky factor
 """
 
 from . import rank_1
-from ._version import version as _version_str
-
-__version__ = _version_str
+from ._version import version as __version__
